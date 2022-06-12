dfdf4e307f955b1c852e6d37661bdde9497c5bfd
Shreevatsa R <shreevatsa.public@gmail.com>
2022-06-12 00:02:33 -0700

    Revert "Saving this (someone has kindly OCR-ed, and the error rate is reasonably small) for later"
    This reverts commit 5d25c686295c89c3b77ee3859483ebf35781c96e.

--- jainqq.sh
+++
@@ -1,2 +0,0 @@
-for i in {1..324}; do echo $i; curl "https://jainqq.org/_next/data/IkRKN-EK5HJsWfnOwjWsA/explore/001450/$i.json?srno=001450&pageno=$i" >> jainqq.json; echo "" >> jainqq.json; sleep 1; done
-cat jainqq.json | jql '"pageProps"."pageText"' > jainqq.md
